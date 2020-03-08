290844058d10a14db78080f8d8c94584d9bc5d6c
SrDeLorean <xebaelvemgador@gmail.com>
2020-03-08 13:21:39 -0300

    Creacion de la clase relaciones
    Esta clase consiste en unir dos genomas con un tipo de relación en especifico, esta relación dado el caso h4 son solamente relaciones matrimonio, convivencia, padre e hijo, entre otros. 
    PD: wololooooo

--- src/app/h04/h04.component.ts
+++ src/app/h04/h04.component.ts
@@ -108,7 +108,7 @@ class Genoma{
 /**
  * 
  */
-class typeOfEntities{
+class typeOfRelation{
   id:number;
   name:string;
   color:string;
@@ -182,3 +182,107 @@ public setFigure(figure:string) {
 
 }
 
+/**
+ * this class allows the creation of relationships between genomes "two genomes only" 
+ * Note: if you need create one relations father and son and mother and son, you need create two relations, but if the relation is gemelos, you not need two relations
+ *       Rememer the father may have a son but he can marrierd other female or male. that woman would not be the mother
+ */
+class relations{
+  id:number;
+  idTypeOfRelation:number;
+  idGenomaA:number;
+  idGenomaB: number;
+  idSubject:number;
+
+  /**
+   * Constructor of the relations
+   * @param id Construc
+   * @param idTypeOfRelation 
+   * @param idGenomaA 
+   * @param idGenomaB 
+   * @param idSubject 
+   */
+  constructor (id: number, idTypeOfRelation: number,idGenomaA: number,idGenomaB: number, idSubject: number){
+    this.id=id;
+    this.idTypeOfRelation=idTypeOfRelation;
+    this.idGenomaA=idGenomaA;
+    this.idGenomaB=idGenomaB;
+    this.idSubject=idSubject;
+  }
+
+  /**
+   * getId
+   */
+  public getId() : number {
+    return this.id;
+  }
+
+  /**
+   *setId
+   *id: number 
+  */
+  public setId(id: number) {
+    this.id=id;
+  }
+
+  /**
+   * getIdTypeOfRelations
+   */
+  public getIdTypeOfRelations() : number {
+    return this.idTypeOfRelation;
+  }
+
+  /**
+   *setIdTypeOfRelations
+   *idTypeOfRelations: number 
+  */
+  public setIdTypeOfRelations(idTypeOfRelation: number) {
+    this.idTypeOfRelation=idTypeOfRelation;
+  }
+
+  /**
+   * getIdGenomaA
+   */
+  public getIdGenomaA() : number {
+    return this.idGenomaA;
+  }
+
+  /**
+   *setId
+   *id: number 
+  */
+ public setIdGenomaA(idGenomaA: number) {
+  this.idGenomaA=idGenomaA;
+}
+
+  /**
+   * getIdGenomaB
+   */
+  public getIdGenomaB() : number {
+    return this.idGenomaB;
+  }
+
+  /**
+   *setId
+   *id: number 
+  */
+  public setIdGenomaB(idGenomaB: number) {
+    this.idGenomaB=idGenomaB;
+  }
+
+  /**
+   * getIdSubject
+   */
+  public getIdSubject() : number {
+    return this.idSubject;
+  }
+
+  /**
+   *setId
+   *id: number 
+  */
+  public setIdSubject(idSubject: number) {
+    this.idSubject=idSubject;
+  }
+
+}
