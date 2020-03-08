2aa27e435568c8ed8f856609dee2e31e558080f1
WorLark <felipe.milla.calquin@gmail.com>
2020-03-08 11:14:56 -0300

    Cascaron sujeto y genoma para H04
    Se creo un ts con los elementos mencionados.

---
+++ src/app/h04/h04.component.ts
@@ -0,0 +1,120 @@
+import { Subject } from "rxjs";
+
+//import { getUrlScheme } from "@angular/compiler";
+//import { summaryFileName } from '@angular/compiler/src/aot/util';
+//import { compileNgModuleFromRender2 } from '@angular/compiler/src/render3/r3_module_compiler';
+//import { Component, OnInit } from '@angular/core';
+//@Component({
+ // selector: 'app-h04',
+  //templateUrl: './h04.component.html',
+ // styleUrls: ['./h04.component.css']
+//})
+//class H04Component  {
+  //constructor() { }
+  //ngOnInit(): void {
+  //}
+//}
+
+class H04Component  {
+  constructor(){
+    //logica
+  }
+
+}
+
+
+//clases
+class subject {
+    id: number;
+    name: String;
+    lastName: String;
+    gender: String;
+    
+    constructor(i: number, na: String, lna: String, g: String ){
+      this.id = i;
+      this.name = na;
+      this.lastName = lna;
+      this.gender = g;
+    }
+    
+    public seteId(v : number) {
+      this.id = v;
+    }
+
+    public setName(v : string) {
+      this.name = v;
+    }
+
+    public setLastName(v : string) {
+      this.name = v;
+    }
+
+    public setGender(v : string) {
+      this.name = v;
+    }
+
+    public getId() : number {
+      return this.id;
+    }
+
+    public getName() : String {
+      return this.name;
+    }
+    public getLastName() : String {
+      return this.lastName;
+    }
+    public getGender() : String {
+      return this.gender;
+    }
+
+}
+
+class Genoma{
+  id: number;
+  sub: subject;
+  tyOfEn: typeOfEntities;
+
+  constructor(i: number, s: subject, t: typeOfEntities){
+    this.id = i;
+    this.sub = s;
+    this.tyOfEn = t;
+  }
+
+  public setId(v : number) {
+    this.id = v;
+  }
+
+  public setSub(v : subject) {
+    this.sub = v;
+  }
+
+  public setTyOfEn(v : typeOfEntities) {
+    this.tyOfEn = v;
+  }
+
+  
+  public getId() : number {
+    return this.id;
+  }
+
+  public getSubG() : subject {
+    return this.sub;
+  }
+
+  public getTyOfEnG() : typeOfEntities {
+    return this.tyOfEn;
+  }
+
+}
+
+//El brayan debe hacer esta wea
+class typeOfEntities{
+
+  constructor(){
+
+  }
+
+
+}
+
+
